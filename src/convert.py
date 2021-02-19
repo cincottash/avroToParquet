79b04d9ed7c773b95732678038581a39929a8567
cincottash <cincotta@udel.edu>
2021-02-19 09:35:58 -0500

    more commenting

--- src/convert.py
+++ src/convert.py
@@ -14,30 +14,25 @@ def avroToCSV(avroDirectory, csvDirectory):
 			head = True
 			count = 0
 
-			#make sure we have permission to write to a file
-			try:
-				csvFile = csv.writer(open(csvDirectory + filename.replace('.avro', '.csv'), 'w+'))
-			except PermissionError:
-				print("Error, don't have permission to write to csv file")
-				exit(0)
-
 			#try to convert avro files to csv, if any exception happens while trying to convert a file, skip to the next file
 			with open(avroDirectory + filename, 'rb') as avroFile:
 				
 				try:
 					avroReader = reader(avroFile)
-
+					csvFile = csv.writer(open(csvDirectory + filename.replace('.avro', '.csv'), 'w+'))
 					for data in avroReader:
 						if head == True:
 							header = data.keys()
 							csvFile.writerow(header)
 							head = False
 						csvFile.writerow(data.values())
-				except Exception:
-					print('Failed to convert file {}\n'.format(avroFile))
+				except ValueError:
+					print('Failed to convert file {}\n'.format(filename))
 					avroFile.close()
 					failedConversions.append(filename)
-					os.remove(csvFile)
+				except PermissionError:
+					print("Error, don't have permission to write to csv file")
+					exit(0)
 					
 					continue
 			    
@@ -64,6 +59,7 @@ def csvToParquet(csvDirectory, parquetDirectory):
 				csvFile.to_parquet(parquetDirectory + filename.replace('.csv', '.parquet'))
 			except Exception:
 				failedConversions.append(filename)
+				os.remove(csvDirectory+filename)
 				continue
 		convertedFileCount += 1
 
--- src/setup.py
+++ src/setup.py
@@ -13,7 +13,10 @@ def dirSetup():
 		if(avroDirectory[len(avroDirectory)-1] != '\\' and avroDirectory[len(avroDirectory)-1] != '/'):
 			raise FileNotFoundError
 	except FileNotFoundError:
-		print('Error, missing avro files direcectory')
+		print('Error, missing avro files direcectory, please include trailing slash')
+		exit(0)
+	except PermissionError:
+		print('Error, mission write privileges on avro directory')
 		exit(0)
 
 	#the output dir is one level above the .avro files dir
@@ -22,28 +25,36 @@ def dirSetup():
 	parquetDirectory = outputDirectory + '/parquetFiles/'
 
 	#if csv dir already exists, clear all the files in it
-	if os.path.isdir(csvDirectory):
-		print('Clearing temp csvFiles directory\n')
-		deletedFileCount = 0
-		for filename in os.listdir(csvDirectory):
-			os.remove(csvDirectory + filename)
-			deletedFileCount += 1
-		print('Deleted {} files from temp csv directory\n'.format(deletedFileCount))
-	else:
-		os.mkdir(csvDirectory)
-		print('Created temp csvFiles directory\n')
+	try:
+		if os.path.isdir(csvDirectory):
+			print('Clearing temp csvFiles directory\n')
+			deletedFileCount = 0
+			for filename in os.listdir(csvDirectory):
+				os.remove(csvDirectory + filename)
+				deletedFileCount += 1
+			print('Deleted {} files from temp csv directory\n'.format(deletedFileCount))
+		else:
+			os.mkdir(csvDirectory)
+			print('Created temp csvFiles directory\n')
+	except PermissionError:
+		print('Error, mission write privileges on csv directory')
+		exit(0)
 	
 	#if the parquet dir already exists, loop through it and delete every .parquet file in it
-	if os.path.isdir(parquetDirectory):
-		print('Deleting existing .parquet files\n')
-		deletedFileCount = 0
-		for filename in os.listdir(parquetDirectory):
-			if(filename.endswith('.parquet')):
-				os.remove(parquetDirectory + filename)
-				deletedFileCount += 1
-		print('Deleted {} .parquet files\n'.format(deletedFileCount))
-	else:
-		os.mkdir(parquetDirectory)  
-		print('Created parquetFiles directory\n')
+	try:
+		if os.path.isdir(parquetDirectory):
+			print('Deleting existing .parquet files\n')
+			deletedFileCount = 0
+			for filename in os.listdir(parquetDirectory):
+				if(filename.endswith('.parquet')):
+					os.remove(parquetDirectory + filename)
+					deletedFileCount += 1
+			print('Deleted {} .parquet files\n'.format(deletedFileCount))
+		else:
+			os.mkdir(parquetDirectory)  
+			print('Created parquetFiles directory\n')
+	except PermissionError:
+		print('Error, mission write privileges on parquet directory')
+		exit(0)
 
 	return avroDirectory, csvDirectory, parquetDirectory
